3239a457fd003fbc9849b4a9a410360c132122fb
Ivan Socolsky <jungans@gmail.com>
2015-06-01 11:07:52 -0300

    add formatAmount fn

--- test/walletutils.js
+++ test/walletutils.js
@@ -229,4 +229,52 @@ describe('WalletUtils', function() {
       signatures.length.should.be.equal(utxos.length);
     });
   });
+
+  describe('#formatAmount', function() {
+    it('should successfully format amount', function() {
+      var cases = [{
+        args: [1, 'bit'],
+        expected: '0',
+      }, {
+        args: [1, 'btc'],
+        expected: '0.000000',
+      }, {
+        args: [0, 'bit'],
+        expected: '0',
+      }, {
+        args: [12345678, 'bit'],
+        expected: '123,457',
+      }, {
+        args: [12345678, 'btc'],
+        expected: '0.123457',
+      }, {
+        args: [12345611, 'btc'],
+        expected: '0.123456',
+      }, {
+        args: [1234567899999, 'btc'],
+        expected: '12,345.679000',
+      }, {
+        args: [12345678, 'bit', {
+          thousandsSeparator: '.'
+        }],
+        expected: '123.457',
+      }, {
+        args: [12345678, 'btc', {
+          decimalSeparator: ','
+        }],
+        expected: '0,123457',
+      }, {
+        args: [1234567899999, 'btc', {
+          thousandsSeparator: ' ',
+          decimalSeparator: ','
+        }],
+        expected: '12 345,679000',
+      }, ];
+
+      _.each(cases, function(testCase) {
+        WalletUtils.formatAmount.apply(this, testCase.args).should.equal(testCase.expected);
+      });
+    });
+  });
+
 });
